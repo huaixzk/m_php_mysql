d81ed50ea8c338323f6689b6f89b245c1f2d85a6
root <root@9b8f72c7-abc8-474f-9dd2-2f6ce8893e7a>
2014-08-15 01:46:09 +0000

    first comminit
    git-svn-id: file:///Users/huaixiaoz/Desktop/php/m@1 9b8f72c7-abc8-474f-9dd2-2f6ce8893e7a

---
+++ results.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Book-O-Rama Search Results</title>
+<meta charset="utf-8">
+</head>
+<body>
+<h1>Book-O-Rama Search Results:</h1>
+
+<?php
+	//create short variable names
+	$searchtype=$_POST['searchtype'];
+	$searchterm=trim($_POST['searchterm']);
+if(!$searchtype || !$searchterm)
+{
+	echo 'You have not entered search details. Please go back and <del>try again</del>.';
+	exit;
+}
+
+if(!get_magic_quotes_gpc())
+{
+	$searchtype = addslashes($searchtype);
+	$searchterm = addslashes($searchterm);
+
+	@ $db = new mysqli('localhost', 'user', '00', 'mydb');
+
+	if(mysqli_connect_errno()){
+		echo 'Error: could not connect to database. Please try again later.';
+		exit;
+	}
+
+	$query = "select * from books where ".$searchtype." like '%".$searchterm."%'";
+	$result = $db->query($query);
+
+	$num_results = $result->num_rows;
+
+echo '<p>Numbers of books found: '.$num_rows."</p>";
+
+	for($i = 0; $i < $num_results; $i++){
+		$row = $result->fetch_assoc();
+		echo "<p><strong>".($i+1).". Title: ";
+		echo htmlspecialchars(stripslashes($row['title']));
+		echo "</strong><br />Author:";
+		echo stripslashes($row['author']);
+		echo "<br />ISBN:";
+		echo stripslashes($row['isbn']);
+		echo "<br />Price: ";
+		echo stripslashes($row['price']);
+		echo "</p>";
+	}
+
+	$result -> free();
+	$db -> close();
+}
+?>
+</body>
+</html>
---
+++ sql/book_insert.sql
@@ -0,0 +1,27 @@
+use mydb;
+
+insert into customers values
+(3, 'Julie Smith', '25 Oak Street', 'Airport West'),
+(4, 'Alan Wong', '1/47/Haines Avenue', 'Box Hill'),
+(5, 'Michelle Arthur', '357 North Road', 'Yarraville');
+
+insert into orders values
+(NUll, 3, 69.98, '2007-04-02'),
+(NULL, 1, 49.99, '2007-04-15'),
+(NULL, 2, 74.98, '2007-04-19'),
+(NULL, 3, 24.99, '2007-05-01');
+
+insert into books values
+('0-672-31607-8', 'Michael Morgan', 'Java 2 for professional Developers', 34.99),
+('0-672-31715-1', 'Thomas Down', 'Installng Debian GNU?Linux', 24.99),
+('0-672-31579-2', 'Pruitt, et al', 'Teach Yourself GIMP in 24 Hours', 24.99),
+('0-672-31769-9', 'Thomas Schenk', 'Caldera OpenLinux System Administratior Unleashed', 49.99);
+
+insert into order_items values
+(1, '0-672-32697-8', 2),
+(2, '0-672-31769-9', 1),
+(3, '0-672-32769-9', 1),
+(4, '0-672-33745-1', 3);
+
+insert into book_reviews values
+('0-672-31697-8', 'The Morgan Vook is clearly written and goes well beyond most of the basic Java books out there.');
