e4117e1b626696604b231cb40a2a9a672ed76c3d
root <root@9b8f72c7-abc8-474f-9dd2-2f6ce8893e7a>
2014-08-15 05:15:11 +0000

    fixed svnbak.sh to backup to the /var/ftp/svn
    git-svn-id: file:///Users/huaixiaoz/Desktop/php/m@8 9b8f72c7-abc8-474f-9dd2-2f6ce8893e7a

--- svnbak.sh
+++ svnbak.sh
@@ -2,6 +2,11 @@
 DES=/svn
 DES0=/var/www/html
 M=m
+TO=/var/ftp/svn
+
+CP=`which cp`
+TAR=`which tar`
+
 if [ -e /bin/xz ]
 then
 FLAGS=-cJf
@@ -11,7 +16,8 @@ FLAGS=-cjf
 TAR=tar.bz2
 fi
 OUT=svn_$M.$TAR
-echo $OUT
+#echo $OUT debug
+
 if [ "$PWD" != "$DES" ];
 then
 	echo "passed"
@@ -26,4 +32,29 @@ if [ -d $M ];
       then
       tar $FlAGS $OUT $M
       fi
+fi
+
+if [ -e $OUT ];
+then 
+	$CP $OUT $TO
+	if [ $? == '0' ];
+		then 
+		echo "$CP $OUT to $TO successed"
+	else
+		echo "$CP $OUT failed , check on it, dude ."
+	fi
+fi
+
+# without svn, then tar all source not repo.
+#  done
+
+BAK=php_mysql.$TAR
+cd /var/www/html
+tar $FLAGS $BAK $M 
+
+if [ $? == '0' ];
+then 
+	$CP $BAK $TO && echo "$CP $BAK to $TO successed"
+else 
+	echo "Tar $BAK failed, check it , dude !!"
 fi
