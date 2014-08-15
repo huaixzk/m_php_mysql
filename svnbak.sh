7c80db5ab9ccbda9216d5921e907ddf47535d1db
root <root@9b8f72c7-abc8-474f-9dd2-2f6ce8893e7a>
2014-08-15 02:55:49 +0000

    fixed with xz
    git-svn-id: file:///Users/huaixiaoz/Desktop/php/m@4 9b8f72c7-abc8-474f-9dd2-2f6ce8893e7a

--- svnbak.sh
+++ svnbak.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 DES=/svn
 M=m
+if [ -e /bin/xz ]
+then
+FLAGS=-cJf
 TAR=tar.xz
+else
+FLAGS=-cjf
+TAR=tar.bz2
+fi
 OUT=svn_$M.$TAR
 echo $OUT
 if [ "$PWD" != "$DES" ];
@@ -10,12 +17,12 @@ then
 	cd $DES
 	if [ -d $M ];
 	then
-	tar -cJf $OUT $M 
+	tar $FLAGS $OUT $M 
 	fi
 else
 echo "already in $DES"
 if [ -d $M ];
       then
-      tar -cJf $OUT $M
+      tar $FlAGS $OUT $M
       fi
 fi
