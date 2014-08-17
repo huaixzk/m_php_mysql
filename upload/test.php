4a8e0052423233735be0eb2162ac08d6fa6f3ea0
root <root@9b8f72c7-abc8-474f-9dd2-2f6ce8893e7a>
2014-08-17 16:58:19 +0000

    fixed test.php for
    git-svn-id: file:///Users/huaixiaoz/Desktop/php/m@28 9b8f72c7-abc8-474f-9dd2-2f6ce8893e7a

--- upload/test.php
+++ upload/test.php
@@ -1,5 +1,17 @@
 <?php
-	echo $_SERVER['SERVER'];
+	echo "<ul>";
+	foreach( $_SERVER as $i => $j)
+	{
+		echo "<li><i>$i</i></li>";
+	//	echo "<li><b>$j</b></li>";
+	//	echo "<ol>";
+	//	var_dump($j);
+	//	echo "</ol>";
+	}
 
+	echo "</ul><hr>";
+	
+	echo count($_SERVER)."<hr>";
+	var_dump(disk_free_space($_SERVER['DOCUMENT_ROOT'])/1024);
 
 ?>
